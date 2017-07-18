4071e5672c9286467cb586c404e40f4bdbfc50c2
Leo Silverberg <leosilverberg@gmail.com>
2017-07-18 11:09:43 +0100

    json test14

--- scripts/camera_stream.py
+++ scripts/camera_stream.py
@@ -135,7 +135,7 @@ class ControlThread(Thread):
 			data = sys.stdin.readline()
 			if data > "" :
 				dataString = "str(data)"
-				print(data)
+				print(data["type"])
 				if dataString == "up\n" :
 					print("[py] got up")
 					raStepper.step(1,Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
