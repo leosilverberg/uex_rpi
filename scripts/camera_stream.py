85383dcff721302ad1371198818ec65b9bd75d34
Leo Silverberg <leosilverberg@gmail.com>
2017-07-18 15:31:46 +0100

    camera floating

--- scripts/camera_stream.py
+++ scripts/camera_stream.py
@@ -189,7 +189,7 @@ class ControlThread(Thread):
 						print('{"msg":"changed framerate"}')
 						print('{"msg":"changing exposure"}')
 						
-						print('{"msg":"'+int(EXPOSURE_TIME)*10+'"}')
+						print('{"msg":"'+float(str(EXPOSURE_TIME))*10+'"}')
 
 						camera.shutter_speed = float(EXPOSURE_TIME)*1000000
 						print('{"msg":"changed exposure"}')
