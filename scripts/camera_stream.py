1df3817bc060929a220d0056e841fc25d1c4a6f0
Leo Silverberg <leosilverberg@gmail.com>
2017-07-19 15:53:46 +0100

    exposure range

--- scripts/camera_stream.py
+++ scripts/camera_stream.py
@@ -203,7 +203,7 @@ class ControlThread(Thread):
 						print('{"msg":"stopped recording"}')
 						camera.resolution = (2592,1944)
 						print('{"msg":"set to high-res"}')
-						camera.framerate = (Fraction(1,6))
+						camera.framerate = 1/float(EXPOSURE_TIME)
 						print('{"msg":"changed framerate"}')
 						print('{"msg":"changing exposure"}')
 
@@ -234,6 +234,7 @@ class ControlThread(Thread):
 							print('{"msg":"some error occured in setting the shutterspeed========================"}')
 
 						print('{"msg":"exposure speed:'+str(timedelta(microseconds=camera.shutter_speed))+'"}')
+
 						camera.exposure_mode = 'off'
 						print('{"msg":"changed exposure"}')
 						camera.iso = int(ISO)
